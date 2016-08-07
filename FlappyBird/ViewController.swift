d0855e3d30c282b5adbf9dd8202ade12a10af00f
yasuhito <yasuhito@nagatayasuhito-no-macbook-air.local>
2016-08-07 21:47:57 +0900

    課題前の状態

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -29,7 +29,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 }
 
